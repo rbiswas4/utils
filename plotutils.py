8324ca91f301e245d73613c9083c3de7f2dfda0c
Rahul Biswas <rbiswas4@gmail.com>
2014-04-08 12:06:13 -0500

    Added formatter to plotutils so that offsets are not added

--- plotutils.py
+++ plotutils.py
@@ -8,32 +8,57 @@ import matplotlib.gridspec as gridspec
 def settwopanel( height_ratios = [1.0,0.3], 
 	width_ratios = [1.,0.] ,
 	padding = None, 
+	setoffset = None, 
 	figsize  = None ):
 
 	"""
-	returns 
-	args:
-		height_ratios:
+	returns a figure and axes for a main panel and a lower panel for 
+	showing differential information of overplotted quantities in
+	the top panel. 
+	args	:
+		height_ratios: list of floats, optional defaults to 
+				[1.0, 0.3]
+			height ratio between the upper and lower panel 
 
-		width_ratios :
+		width_ratios :list of floats, optional defaults to 
+				[1.0, 0.0]
+			width ratio between the left and right  panel 
 		
 		figsize: 
 
-	returns:
+	returns :
+		figure object , ax0 (axes for top panel) , and ax1 
+			(axes for lower panel)
 
-	usage:
+	usage   :
+		>>> myfig,  myax0 , myax1 = settwopanel ( )
+		>>> myax0.plot( x,  y) 
+		>>> myax1.plot(x, x)
+		>>> myfig.tight_layout()
 
+	status  :
+
+		tested by 
+		R. Biswas, Fri Feb 21 00:52:55 CST 2014
 	"""
+	import matplotlib.ticker as ticker
+	majorformatter = ticker.ScalarFormatter(useOffset =False)
 
-	hpad  = 0.0 
-	fig = plt.figure()
+	if figsize == None:
+		fig = plt.figure()
+	else:
+		fig = plt.figure(figsize = figsize)
+	
+
 	gs = gridspec.GridSpec ( 2, 1, width_ratios = width_ratios , height_ratios = height_ratios)
 
 	ax0 = plt.subplot(gs[0]) 
 	ax1 = plt.subplot(gs[1])
+	ax0.set_xticklabels("",visible = False)
+	ax1.yaxis.set_major_formatter(majorformatter)
 
-	
-	gridspec.update(hpad = hpad)
+	hpad  = 0.0 
+	#gridspec.update(hpad = hpad)
 	return fig , ax0 , ax1
 
 
