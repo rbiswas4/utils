b6cb7cae9111336ce6b0529646b957b5bdfcd2d2
Rahul Biswas <rbiswas4@gmail.com>
2014-04-18 08:31:39 -0500

    Added drawxband to plotutils

--- plotutils.py
+++ plotutils.py
@@ -1,10 +1,82 @@
 #!/usr/bin/env python
 
+import typeutils  as tu
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 
+def drawxband(refval , 
+	xlims = None, 
+	color = 'gray',
+	bandwidths = [-0.1,0.1], 
+	ua = None ):
+	
+	"""
+	draw a shaded band of color color of certain width around a single 
+	reference value refval
+	args:
+		refval    :	float , mandatory
+			scalar (single) reference value, 
+			Example: refval  = 1.0
+			
+		xlims     : list of two floats, optional, defaults to None
+			min and max x values through which the band will 
+			be drawn
+			if None, these values are set from the limits of the 
+			supplied axessubplot. If axessubplots is not supplied
+			this will raise an error
+				
+		color     : python color style, optional,defaults to 'gray' 
+			color of the band
+		bandwidths : list of two floats, optional default to [-0.1, 0.1]
+			width of the band to be shaded. 
+		ua	  : optional, axes.subplot object on which to draw, 
+			defaults to None
+			axes object to use
+	returns:
+		axes object
+	example usage:
+		>>> #To use an existing axes subplot object
+		>>> drawxband (refval = 60., bandwidths = [-20.,20.], color = 'green', ua = myax0)	
+		>>> #No axes object is available
+		>>> drawxband (refval = 60., xlims = [4., 8.] , bandwidths = [-20.,20.],color = 'green')	
+
+	status: Tests in plot_utils main. 
+		tested R. Biswas, Fri Apr 18 08:29:54 CDT 2014 
+
+	"""
+
+	if ua == None:  
+		ua = plt.gca()
+	else:
+		plt.sca(ua)
+		xl ,xh = ua.get_xlim()
+		if xlims == None:
+			xlims = [ xl , xh ] 
+
+
+	#really started this conditional statement to make it general, 
+	#but have not finished, will always go to the else condition
+	#in current implementation
+	if xlims == None:
+		if not tu.isiterable(refval):
+			raise ValueError("To draw band supply either refval as numpy array or xlims over which it should be drawn")
+				
+	else:
+		xvals = np.linspace(xlims[0], xlims[1],2)
+
+			#refvals = refval *np.ones(len(xvals) 
+
+
+
+	#plot reference value
+	ua.axhline(refval,color ='k',lw =2.0)
+	#draw band
+	ua.fill_between (xvals , refval + bandwidths[0], refval + bandwidths[1],
+		color  = color , alpha =  0.25) 
+
+	return ua
 def settwopanel( height_ratios = [1.0,0.3], 
 	width_ratios = [1.,0.] ,
 	padding = None, 
@@ -72,7 +144,14 @@ if __name__ == "__main__":
 	myfig,  myax0 , myax1 = settwopanel ( )
 
 	myax0.plot( x,  y) 
-	myax1.plot(x, x)
+	drawxband (refval = 60.,bandwidths = [-20.,20.],color = 'green', ua = myax0)	
+	#myax1.plot(x, x)
 	
 	myfig.tight_layout()
+
+	plt.figure()
+	plt.plot(x, y)
+	drawxband (refval = 60., xlims = [4., 8.] , bandwidths = [-20.,20.],color = 'green')	
+	
+	
 	plt.show()
